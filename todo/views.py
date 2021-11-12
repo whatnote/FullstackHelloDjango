e90dc62c2e46dbe7b93104fe0a9614d7cc9a32cd
DANIEL <daniel_cornwall@hotmail.com>
2021-11-12 10:27:37 +0000

    adding form

---
+++ todo/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from .models import Item
+from .forms import ItemForm
+# Create your views here.
+
+
+def get_todo_list(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    if request.method == 'POST':
+        name = request.POST.get('item_name')
+        done = 'done' in request.POST
+        Item.objects.create(name=name, done=done)
+
+        return redirect('get_todo_list')
+    form = ItemForm()
+    context = {
+        'form':form
+    }
+    return render(request, 'todo/add_item.html', context)
